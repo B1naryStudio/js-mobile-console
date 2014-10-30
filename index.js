224b1c0604e5968c5330973ada47403ce0651983
msemenistyi <nikeiwe@gmail.com>
2014-10-30 18:08:02 +0200

    add options showOnError, proxyConsole

--- index.js
+++ index.js
@@ -23,16 +23,36 @@
 	'';
 
 	var jsConsole = {
+		props: {
+			showOnError: false,
+			proxyConsole: true
+		},
+
+		init: function(){
+			if (!this.initialized){
+				this.bindErrorListener();
+				this.initializeContainers();
+				this.bindListeners();
+				this.initialized = true;
+			}
+		},
+
+		options: function(options){
+			for (var i in options){
+				if (typeof this.props[i] !== 'undefined'){
+					this.props[i] = options[i];
+				}
+			}
+			this.init();
+		},
+
 		show: function(){
 			var el = document.getElementById('js-mobile-console');
 			if (!el){
-				this.$el = {};
-				el = this.$el.container = document.createElement('div');
-				el.id = 'js-mobile-console';
-				el.innerHTML = containerHtml;
-				document.body.appendChild(el);
-				this.initializeContainers();
-				this.bindListeners();
+				this.init();
+				if (this.props.proxyConsole){
+					this.decorateConsole();
+				}
 			}
 			el.style.display = 'block';
 		},
@@ -50,6 +70,13 @@
 		},
 
 		initializeContainers: function(){
+			this.$el = {};
+			el = this.$el.container = document.createElement('div');
+			el.id = 'js-mobile-console';
+			el.innerHTML = containerHtml;
+			el.style.display = 'none';
+			document.body.appendChild(el);
+
 			this.$el.input = document.getElementById('jsmc-input');
 			this.$el.button = document.getElementById('jsmc-button');
 			this.$el.log = document.getElementById('jsmc-log');
@@ -59,39 +86,74 @@
 			var self = this;
 			this.$el.button.addEventListener('click', function(){
 				var val = self.$el.input.value;
-				var logEl = document.createElement('div');
-				logEl.className = 'jsmc-log-el';
-				logEl.innerHTML = logElementHtml;
 				var text;
+				var error;
 				try {
 					text = window.eval(val);
 				} catch (e){
-					logEl.className += ' jsmc-log-error';
 					text = e;
+					error = true;
 				}
-				var logTextEl = logEl.getElementsByClassName('jsmc-log-text')[0];
-				logTextEl.innerHTML = text;
-				self.appendLogEl(logEl);
+				self.logValue(text, error);
 			});
+		},
 
-			window.onerror = function(erorMessage, file, lineNumber, columnNumber){
-				var logEl = document.createElement('div');
-				logEl.className = 'jsmc-log-error jsmc-log-el';
-				logEl.innerHTML = logElementHtml;
-				var logTextEl = logEl.getElementsByClassName('jsmc-log-text')[0];
-				var logTargetEl = logEl.getElementsByClassName('jsmc-log-target')[0];
-				logTextEl.innerHTML = erorMessage;
-				logTargetEl.innerHTML = file + ':' + lineNumber + (columnNumber ? (
-					':' + columnNumber) : '');
-				self.appendLogEl(logEl);
+		bindErrorListener: function(){
+			var self = this;
+			window.onerror = function(errorMessage, file, lineNumber, columnNumber){
+				if (self.props.showOnError){
+					self.show();
+				}
+				var error = file + ':' + lineNumber + (columnNumber ? (':' + columnNumber) : '');
+				self.logValue(errorMessage, error);
 			};
 		},
 
 		appendLogEl: function(el){
 			this.$el.log.appendChild(el);
 			this.$el.log.scrollTop = this.$el.log.scrollHeight;
+		},
+
+		decorateConsole: function(){
+			var self = this;
+			if (this.consoleDecorated){
+				return;
+			}
+			this.consoleDecorated = true;
+			if (window.console){
+				if (window.console.log){
+					var log = window.console.log;
+					window.console.log = function(){
+						var args = [].slice.call(arguments);
+						log.apply(window.console, args);
+						self.logValue(args.join(' '));
+					};
+				}
+			}
+		}, 
+
+		logValue: function(value, error){			
+			var logEl = document.createElement('div');
+			logEl.className = 'jsmc-log-el';
+			logEl.innerHTML = logElementHtml;
+
+			if (error){
+				logEl.className += ' jsmc-log-error';
+			}
+
+			var logTextEl = logEl.getElementsByClassName('jsmc-log-text')[0];
+			logTextEl.innerHTML = value;
+
+			if (typeof error === 'string'){
+				var logTargetEl = logEl.getElementsByClassName('jsmc-log-target')[0];
+				logTargetEl.innerHTML = error;
+			}
+
+			this.appendLogEl(logEl);
 		}
 	};
+
+	jsConsole.decorateConsole();
 
 	return jsConsole;
 

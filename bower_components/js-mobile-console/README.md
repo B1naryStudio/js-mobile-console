f693c8621e0864ba1247c33787737795bf0c9aab
Mykyta Semenistyi <nikeiwe@gmail.com>
2014-11-01 19:46:37 +0200

    initial commit

---
+++ bower_components/js-mobile-console/README.md
@@ -0,0 +1,120 @@
+js-mobile-console
+=====
+
+![js-mobile-console](mobile_screen.png)
+
+## Install
+**Zip archive**:
+[Download](https://github.com/msemenistyi/js-mobile-console/archive/master.zip)
+
+**Browserify**:
+> npm install js-mobile-console
+
+[![NPM](https://nodei.co/npm/js-mobile-console.png)](https://nodei.co/npm/js-mobile-console/)
+
+**Bower**:
+> bower install js-mobile-console
+
+##Overview
+
+[Live demo](http://msemenistyi.github.io/js-mobile-console/).
+
+It is extremely nice, that we are on the road of specifying and unifying remote
+debugging protocol and we can already remotely debug web applications on desktop 
+chrome, but what if we want to get it working also on Android browser or 
+Safari for iOs?
+
+MobileConsole can be embedded within any page for debugging, it will catch errors
+and behave exactly as native js console in browser. It also outputs all the logs
+you've written via an API of window.console. 
+
+###Loading
+Css file should be included within html:
+<link rel="stylesheet" href="./style/mobile-console.min.css">
+
+Console may be used without any modular system:
+```html
+<script src="./mobile-console.min.js"></script>
+```
+
+With help of browserify:
+```js
+	var mobileConsole = require('js-mobile-console');
+```
+
+Or AMD style (RequireJS): 
+```js
+	define(['js-mobile-console'], function(mobileConsole))
+```
+
+###Usage
+
+Simple usage:
+```js
+	mobileConsole.show();
+```
+
+Advanced usage: 
+```js
+	mobileConsole.options({
+		showOnError: true,
+		proxyConsole: false,
+		isCollapsed: true,
+		catchErrors: true
+	});
+```
+
+Conditional toggling:
+```js
+	if (condition){
+		mobileConsole.show();
+	} else {
+	mobileConsole.hide();
+}
+```
+
+###API
+
+- **show** - show console with default options.
+- **hide** - hide console.
+- **options** - method to initialize console, by default will show console, 
+accepts hash of options.
+
+###Options
+
+- **showOnError** - *Default* false. Console will be hidden if no show method 
+was called, but it will appear if any error occurs.
+- **proxyConsole** - *Default* true. Determines if window.console methods are 
+proxied to mobile console.
+- **isCollapsed** - *Default* false. Determines if Console is collpased on startup.
+- **catchErrors** - *Default* false. Determines if Console is registring 
+window.onerror method in order to catch errors.
+
+
+##Contributing
+Feel free to open issues and send PRs. 
+
+## License
+
+The MIT License (MIT)
+
+[![Binary Studio](http://www.binary-studio.com/images/logo.gif)](http://binary-studio.com)  
+Copyright (c) 2014 Semenistyi Mykyta nikeiwe@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ bower_components/js-mobile-console/mobile-console.js
@@ -0,0 +1,229 @@
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	} else {
+		root.mobileConsole = factory();
+	}
+})(this, function () {
+
+	var containerHtml = '' + 
+	'<div id="jsmc-collapse"></div>' +
+	'<div id="jsmc-clear">&#xd7</div>' +
+	'<div id="jsmc-content">' +
+	'	<input id="jsmc-button" type="button" value="Run"/>' +
+	'	<div id="jsmc-log">' +
+	'	</div>' +
+	'	<div id="jsmc-input-container">' +
+	'		<input id="jsmc-input" type="text" placeholder="type your js here"/>' +
+	'	</div>' +
+	'</div>' +
+	'';
+
+	var logElementHtml = '' +
+	'	<div class="jsmc-log-text"></div>' +
+	'	<div class="jsmc-log-target"></div>' +
+	'';
+
+	var mobileConsole = {
+		props: {
+			showOnError: false,
+			proxyConsole: true,
+			isCollapsed: false,
+			catchErrors: true
+		},
+
+		init: function(){
+			if (!this.initialized){
+				if (this.props.catchErrors){
+					this.bindErrorListener();
+				}
+				this.initializeContainers();
+				this.bindListeners();
+				this.initialized = true;
+
+				if (this.props.proxyConsole){
+					this.decorateConsole();
+				} else {
+					this.undecorateConsole();
+				}
+			}
+		},
+
+		options: function(options){
+			for (var i in options){
+				if (typeof this.props[i] !== 'undefined'){
+					this.props[i] = options[i];
+				}
+			}
+			this.init();
+		},
+
+		show: function(options){
+			var el = document.getElementById('js-mobile-console');
+			if (!el){
+				this.init();
+			}
+			this.$el.container.style.display = 'block';
+
+			if (options && options.expand){
+				this.toggleCollapsed(false);
+			}
+		},
+
+		hide: function(){
+			if (this.$el && this.$el.container){
+				this.$el.container.style.display = 'none';
+			}
+		},
+
+		destroy: function(){
+			var el = document.getElementById('js-mobile-console');
+			el.parentNode.removeChild(el);
+		},
+
+		initializeContainers: function(options){
+			this.$el = {};
+			el = this.$el.container = document.createElement('div');
+			el.id = 'js-mobile-console';
+			el.innerHTML = containerHtml;
+			el.style.display = 'none';
+			document.body.appendChild(el);
+
+			this.$el.input = document.getElementById('jsmc-input');
+			this.$el.button = document.getElementById('jsmc-button');
+			this.$el.log = document.getElementById('jsmc-log');
+			this.$el.content = document.getElementById('jsmc-content');
+			this.$el.collapseControl = document.getElementById('jsmc-collapse');
+			this.$el.clearControl = document.getElementById('jsmc-clear');
+
+			if (this.props.isCollapsed){
+				this.$el.content.style.display = 'none';
+				this.isCollapsed = true;
+				this.$el.collapseControl.innerHTML = '&#9650;';
+			} else {
+				this.$el.collapseControl.innerHTML = '&#9660;';				
+			}
+		},
+
+		toggleCollapsed: function(toBeCollapsed){
+			this.isCollapsed = typeof toBeCollapsed === 'boolean' ? toBeCollapsed : !this.isCollapsed;
+			var display = this.isCollapsed ? 'none' : 'block';
+			this.$el.content.style.display = display;
+			this.$el.collapseControl.innerHTML = this.isCollapsed ? '&#9650;' : '&#9660;';
+			if (this.isCollapsed){
+				this.$el.clearControl.style.display = 'none';
+			} else {
+				this.$el.clearControl.style.display = 'block';
+			}
+		},
+
+		bindListeners: function(){
+			var self = this;
+			this.$el.collapseControl.addEventListener('click', function(){
+				self.toggleCollapsed();
+			});
+
+			this.$el.clearControl.addEventListener('click', function(){
+				self.clearLogs();
+			});
+
+			this.$el.button.addEventListener('click', function(){
+				logValue();
+			});
+
+			this.$el.input.addEventListener('keyup', function(e){
+				if (e.which === 13){
+					logValue();
+				}
+			});
+
+			function logValue(){
+				var val = self.$el.input.value;
+				var text;
+				var error;
+				try {
+					text = window.eval(val);
+				} catch (e){
+					text = e;
+					error = true;
+				}
+				self.logValue(text, error);
+			}
+		},
+
+		clearLogs: function(){
+			this.$el.log.innerHTML = '';
+		},
+
+		bindErrorListener: function(){
+			var self = this;
+			window.onerror = function(errorMessage, file, lineNumber, columnNumber){
+				if (self.props.showOnError){
+					self.show({expand: true});
+				}
+				var error = file + ':' + lineNumber + (columnNumber ? (':' + columnNumber) : '');
+				self.logValue(errorMessage, error);
+			};
+		},
+
+		appendLogEl: function(el){
+			this.$el.log.appendChild(el);
+			this.$el.log.scrollTop = this.$el.log.scrollHeight;
+		},
+
+		decorateConsole: function(){
+			var self = this;
+			if (this.consoleDecorated){
+				return;
+			}
+			this.consoleDecorated = true;
+			if (window.console){
+				if (window.console.log){
+					this.oldLog = window.console.log;
+					window.console.log = function(){
+						var args = [].slice.call(arguments);
+						self.oldLog.apply(window.console, args);
+						self.logValue(args.join(' '));
+					};
+				}
+			}
+		}, 
+
+		undecorateConsole: function(){
+			var self = this;
+			if (this.consoleDecorated){
+				window.console.log = function(){
+					var args = [].slice.call(arguments);
+					self.oldLog.apply(window.console, args);
+				};
+			}
+		},
+
+		logValue: function(value, error){			
+			var logEl = document.createElement('div');
+			logEl.className = 'jsmc-log-el';
+			logEl.innerHTML = logElementHtml;
+
+			if (error){
+				logEl.className += ' jsmc-log-error';
+			}
+
+			var logTextEl = logEl.getElementsByClassName('jsmc-log-text')[0];
+			logTextEl.innerHTML = value;
+
+			if (typeof error === 'string'){
+				var logTargetEl = logEl.getElementsByClassName('jsmc-log-target')[0];
+				logTargetEl.innerHTML = error;
+			}
+
+			this.appendLogEl(logEl);
+		}
+	};
+
+	mobileConsole.decorateConsole();
+
+	return mobileConsole;
+
+});
